f72c14f8912274fa3c4e7ca73a61618b20ef7fe0
Eduardo5011 <eduardo.rosado5011@gmail.com>
2021-11-12 22:36:13 -0500

    update

--- app/blueprints/shop/routes.py
+++ app/blueprints/shop/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, request
+from app.models import Item, Category
 
 
 from flask_login import login_required
@@ -16,6 +17,35 @@ def view():
 @login_required  
 def cart():
     return render_template('cart.html.j2')
+
+
+@shop.route('/checkout', methods=['GET'])
+@login_required
+def checkout():
+    return render_template('checkout.html.j2')
+
+
+@shop.route('/add_item', methods=['GET'])
+def add_item():
+    item_id = request.get('id')  
+    item = Item.query.filter_by(id=item_id).first()
+    item.add_item()
+    return  render_template('cart.html.j2')
+
+
+@shop.route('/remove_item',  methods=['GET'])
+def remove_item():
+    item_id = request.get('id')  
+    item = Item.query.filter_by(id=item_id).first()
+    item.remove_item()
+    return  render_template('cart.html.j2')
+
+@shop.route('/view_item',  methods=['GET'])
+def view_item():
+    item_id = request.get('id')  
+    item = Item.query.filter_by(id=item_id).first()
+    item.view_item()
+    return  render_template('cart.html.j2')
 
 
 
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import db
-from flask_login import UserMixin 
+from flask_login import UserMixin, current_user
 from datetime import datetime as dt
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
@@ -11,8 +11,8 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(150))
     email = db.Column(db.String(200), unique=True)
     password = db.Column(db.String(200))
-    # created_on = db.Column(db.DateTime, index=True, default=dt.utcnow)
-    # category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    created_on = db.Column(db.DateTime, index=True, default=dt.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
     def __repr__(self):
         return f'<User: {self.id} | {self.email}>'
@@ -45,77 +45,101 @@ def load_user(id):
 
 
 
-# class Item(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(200))
-#     description = db.Column(db.Text)
-#     price = db.Column(db.Float)
-#     img = db.Column(db.String)
-#     created_on = db.Column(db.DateTime, index=True, default=dt.utcnow)
-#     category_id = db.Column(db.ForeignKey('category.id'))
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    description = db.Column(db.Text)
+    price = db.Column(db.Float)
+    img = db.Column(db.String)
+    created_on = db.Column(db.DateTime, index=True, default=dt.utcnow)
+    category_id = db.Column(db.ForeignKey('category.id'))
 
-#     def __repr__(self):
-#         return f'<Item: {self.id} | {self.name}>'
+    def __repr__(self):
+        return f'<Item: {self.id} | {self.name}>'
 
-#     def save(self):
-#         db.session.add(self)
-#         db.session.commit()
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-#     def delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
-#     def to_dict(self):
-#         data={
-#             'id':self.id,
-#             "name":self.name,
-#             "price":self.price,
-#             "img":self.img,
-#             "description":self.description,
-#             "category_id":self.category_id,
-#             "created_on":self.created_on,
-#         }
-#         return data
-#     def from_dict(self, data):
-#         for field in ["name","description","price","img","category_id"]:
-#             if field in data:
-#                 setattr(self,field, data[field])
+    def to_dict(self):
+        data={
+            'id':self.id,
+            "name":self.name,
+            "price":self.price,
+            "img":self.img,
+            "description":self.description,
+            "category_id":self.category_id,
+            "created_on":self.created_on,
+        }
+        return data
+    def from_dict(self, data):
+        for field in ["name","description","price","img","category_id"]:
+            if field in data:
+                setattr(self,field, data[field])
             
 
 
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    products = db.relationship('Item', cascade ='all, delete-orphan',  backref="category")
 
 
+    def __repr__(self):
+        return f'<Category: {self.id} | {self.name}>'
 
 
-# class Category(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(200))
-#     products = db.relationship('Item', cascade ='all, delete-orphan',  backref="category")
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
-#     def __repr__(self):
-#         return f'<Category: {self.id} | {self.name}>'
+    def to_dict(self):
+        data={
+            "id": self.id,
+            "name": self.name
+        }
+        return data
 
 
-#     def save(self):
-#         db.session.add(self)
-#         db.session.commit()
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.ForeignKey('item.id'))
+    user_id = db.Column(db.ForeignKey('user.id'))
 
-#     def delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
+    def from_dict(self, data):
+        for field in ["item_id", "user_id"]:
+            if field in data:
+                setattr(self,field, data[field])
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-#     def to_dict(self):
-#         data={
-#             "id": self.id,
-#             "name": self.name
-#         }
-#         return data
+    def add_item(self, item):
+        data= {
+            "item_id": item.id,
+            "user_id": current_user.id,
 
+        }
+        self.from_dict(data)
+        self.save()
 
-
+    def remove_item(self, item_id):
+        Cart.query.filter(Cart.user_id == current_user.id and Cart.item_id == item_id).first().delete()
 
 
     
